5c9ab738f4da5011a0345d01d44c0505dcd7ab90
unknown_haribda <bekza.bekmuratov@gmail.com>
2021-02-01 12:55:47 +0600

    Title changed;

--- resources/views/modules/admin/parts/head.blade.php
+++ resources/views/modules/admin/parts/head.blade.php
@@ -1,4 +1,4 @@
-<title>Transportation</title>
+<title>Kulager service</title>
 <meta charset="utf-8">
 {{--<link rel="icon" href="{{asset('images/education_icon.svg')}}">--}}
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
